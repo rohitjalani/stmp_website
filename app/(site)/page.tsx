17e309e9ffe1fa06a1b5f8197b20a1877c974ee1
unknown <rohitjalanirj@gmail.com>
2024-05-27 13:08:28 +0530

    someup

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import Hero from "@/components/Hero";
-import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
   title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
